e7b28f8a86d5cacd7e8d7b1084a621dc4dbe6a27
Andrzej Osuchowski <osuchto@wp.pl>
2018-09-22 10:34:15 +0200

    IE open

---
+++ PMT/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim IE As Object
+
+        IE = CreateObject("InternetExplorer.Application")
+
+        IE.Visible = True
+        IE.Navigate("http://www.onpallet.com/")
+
+        ' Clean up
+        IE.Quit
+        IE = Nothing
+    End Sub
+
+End Module
